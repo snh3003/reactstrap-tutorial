b4f9c74c3a800498860de575979b266b435022f0
snh3003 <shahsama542@gmail.com>
2020-07-11 17:14:02 +0530

    Updates

---
+++ precache-manifest.b05979916ae844b66ee9f2187907c693.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e00e51e30d3038cb66d32ac5f84250",
+    "url": "/reactstrap-tutorial/index.html"
+  },
+  {
+    "revision": "24166db37515ca3e4d1a",
+    "url": "/reactstrap-tutorial/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "24166db37515ca3e4d1a",
+    "url": "/reactstrap-tutorial/static/js/2.ab202e66.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/reactstrap-tutorial/static/js/2.ab202e66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1b5562c75e2639fc33d",
+    "url": "/reactstrap-tutorial/static/js/main.e7d03b2b.chunk.js"
+  },
+  {
+    "revision": "d54f0f437d05e7578c0b",
+    "url": "/reactstrap-tutorial/static/js/runtime-main.abffbad7.js"
+  }
+]);
